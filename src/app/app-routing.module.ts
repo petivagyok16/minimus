b465fb78450ec2622c842db8359d13ec5f801e1a
peter palocz <peter.palocz91@gmail.com>
2018-05-21 11:45:52 +0200

    feat: add more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { DetailsComponent } from './components/details/details.component';
-// import { AddComponent } from './components/add/add.component';
-// import { LoginComponent } from './components/login/login.component';
-// import { SignupComponent } from './components/signup/signup.component';
+import { AddComponent } from './components/add/add.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'details/:city', component: DetailsComponent },
-    // { path: 'add', component: AddComponent },
-    // { path: 'login', component: LoginComponent },
-    // { path: 'signup', component: SignupComponent },
+    { path: 'add', component: AddComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
