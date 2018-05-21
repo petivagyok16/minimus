397a20ad82f9e2c5c53be83f778e67dd531ea386
peter palocz <peter.palocz91@gmail.com>
2018-05-21 11:39:59 +0200

    feat: add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
+// import { AddComponent } from './components/add/add.component';
+// import { LoginComponent } from './components/login/login.component';
+// import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'details/:city', component: DetailsComponent },
+    // { path: 'add', component: AddComponent },
+    // { path: 'login', component: LoginComponent },
+    // { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
