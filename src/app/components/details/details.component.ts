24f2b24c5f655dfe3be89ebc5a65305c724b9560
peter palocz <peter.palocz91@gmail.com>
2018-05-21 11:38:00 +0200

    fix: fix path

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { WeatherService } from '../../../services/weather/weather.service';
+import { WeatherService } from './../../services/weather.service';
 
 @Component({
   selector: 'app-details',
