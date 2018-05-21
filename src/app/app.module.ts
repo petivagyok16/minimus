aebaa7397d06cc353e50779434989804f306ff27
peter palocz <peter.palocz91@gmail.com>
2018-05-21 11:36:20 +0200

    refactor: put components into their own folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
-import { AddCardComponent } from './add-card/add-card.component';
-import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './components/home/home.component';
+import { WeatherCardComponent } from './components/weather-card/weather-card.component';
+import { AddCardComponent } from './components/add-card/add-card.component';
+import { DetailsComponent } from './components/details/details.component';
 import { WeatherService } from './services/weather.service';
 import { UiService } from './services/ui.service';
 
