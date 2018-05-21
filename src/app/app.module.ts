739e5c6f7f1183f2eecd9306f1212f6a40221cb9
peter palocz <peter.palocz91@gmail.com>
2018-05-21 11:22:24 +0200

    feat: add add-card cmp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { AddCardComponent } from './add-card/add-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    WeatherCardComponent
+    WeatherCardComponent,
+    AddCardComponent
   ],
   imports: [
     BrowserModule,
